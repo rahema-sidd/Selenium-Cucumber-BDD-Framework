476be812e6e35d8648a921398557507ac45f14c2
rahema-sidd <20b-079-se@students.uit.edu>
2024-08-25 22:09:38 +0500

    Updated Login ♻️♻️⚒️⚒️

--- src/test/java/StepDefinition/Login.java
+++ src/test/java/StepDefinition/Login.java
@@ -5,11 +5,9 @@ import PageFactory.PageDriver;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-
-import static PageFactory.PF_Functions.waitForSec;
 
 public class Login {
+
     @Given("Start Browser")
     public void startBrowser() {
         PageDriver.launchDriver();
@@ -18,20 +16,21 @@ public class Login {
 
     @Given("Navigate to {string} from page {string} by  Google Chrome")
     public void navigateToFromPageByGoogleChrome(String url, String page) {
+        
         PageDriver.getURL(url, page);
         System.out.println("\n");
     }
 
     @And("User enter {string} on {string} from page {string}")
     public void userEnterOnFromPageByGoogleChrome(String username, String usernameField, String page) throws InterruptedException {
+    
         PF_Functions.sendKeys(username, usernameField, page);
     }
 
     @Then("User click on {string} from page {string}")
     public void userClickOnFromPage(String loginbtn, String page) throws InterruptedException {
+        
         PF_Functions.clickOnElement(loginbtn, page);
-
-
 
     }
 }
