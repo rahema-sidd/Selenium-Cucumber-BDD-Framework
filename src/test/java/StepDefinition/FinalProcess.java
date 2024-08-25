c2d30c7d7079727cb27788b6ed8690df29e84041
rahema-sidd <20b-079-se@students.uit.edu>
2024-08-25 22:16:27 +0500

    Refactored Final Screen 🚀♻️⚒️

--- src/test/java/StepDefinition/FinalProcess.java
+++ src/test/java/StepDefinition/FinalProcess.java
@@ -8,18 +8,21 @@ import io.cucumber.java.en.Then;
 public class FinalProcess {
     @Given("Check if user redirect to page {string} and it matches the {string} from page {string}")
     public void checkIfUserRedirectToPageAndItMatchesTheFromPage(String Finalelement, String expectedData, String page) {
+        
         System.out.println("\nRedirect to "+ expectedData+ "Screen");
         PF_Functions.AssertText(Finalelement, expectedData, page);
     }
 
     @Then("Check to ensure that the message displayed on the {string} matches the expected message {string} in the {string}")
     public void checkToEnsureThatTheMessageDisplayedOnTheMatchesTheExpectedMessageInThe(String msgElement, String expectedMsg, String page) {
+        
         System.out.println("\nValidate Final Message");
         PF_Functions.AssertText(msgElement, expectedMsg, page);
     }
 
     @And("Check if Click on {string} navigates to the Home {string} and it matches the {string} from page {string}")
     public void checkIfClickOnNavigatesToTheHomeAndItMatchesTheFromPage(String homePageBtn, String element, String expected, String page) {
+        
         PF_Functions.clickOnElement(homePageBtn, page);
         System.out.println("\nRedirect back to Product Screen");
         PF_Functions.AssertText(element, expected, page);
