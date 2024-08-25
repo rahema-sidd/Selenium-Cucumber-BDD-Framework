6e513e42c4923639b02314cf7792f9a67ac56fd5
rahema-sidd <20b-079-se@students.uit.edu>
2024-08-25 22:14:46 +0500

    Refactored Hooks for adding SS in Report🚀♻️⚒️

--- src/test/java/StepDefinition/Hooks.java
+++ src/test/java/StepDefinition/Hooks.java
@@ -1,4 +1,4 @@
-package StepDefinition;//package StepDefinition;
+package StepDefinition;
 
 import PageFactory.PageDriver;
 import io.cucumber.java.After;
@@ -8,8 +8,10 @@ import org.openqa.selenium.TakesScreenshot;
 
 
 public class Hooks {
+    
     @After
     public static void tearDown(Scenario scenario) {
+
         TakesScreenshot takesScreenshot = (TakesScreenshot) PageDriver.driver;
         byte[] screenshot = takesScreenshot.getScreenshotAs(OutputType.BYTES);
         scenario.attach(screenshot, "image/png", screenshot.toString());
